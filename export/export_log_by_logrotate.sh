b3ab7c8d1d9a7e67555d05b297ee226a7a00272f
EC2 Default User <ec2-user@ip-172-31-34-178.ap-southeast-1.compute.internal>
2020-10-22 08:27:14 +0000

    와일드 카드 path에 대응

--- export/export_log_by_logrotate.sh
+++ export/export_log_by_logrotate.sh
@@ -1,15 +1,17 @@
+# required args
+# $1 : target path
+# $2 : app name with env
+ 
 # Variables For exporting App Logs
-MY_APP_ROOT=/home/ec2-user/YAC
-RAILS_ENV=staging
-die() { status=$1; shift; echo "FATAL: $*"; exit $status; }
+die() { status=$0; shift; echo "FATAL: $*"; exit $status; }
 EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id || die \"wget instance-id has failed: $?\"`"
-APP_NAME="YAC-staging"
+APP_NAME=$2
 LOG_BUCKET="s3://log.gomi.cloudwatch"
-YES_DAY_STAMP=$(date -d "1 day ago" "+%Y%m%d")
+TIME_STAMP=$(date "+%Y%m%d%H")
 
 TARGET_PATH=$1
 TARGET_FILE=${TARGET_PATH##*/}
-TARGET_DIR=${TARGET_PATH%$TARGET_FILE}
+TARGET_DIR=${TARGET_PATH%"$TARGET_FILE"}
 
-exec "aws" "s3" "cp" "$TARGET_DIR" "$LOG_BUCKET/$APP_NAME/$EC2_INSTANCE_ID/" "--recursive" "--exclude" "*" "--include" "*$TARGET_FILE-$YES_DAY_STAMP*"
+exec "aws" "s3" "cp" "$TARGET_DIR" "${LOG_BUCKET}/${APP_NAME}/${EC2_INSTANCE_ID}/" "--recursive" "--exclude" "*" "--include" "${TARGET_FILE}-${TIME_STAMP}"
 
